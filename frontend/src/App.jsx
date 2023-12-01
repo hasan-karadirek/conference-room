a271147ab08b11caa2d71b948acf243ec2b81c5c
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2023-12-02 00:26:16 +0100

    project created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import MainCarousel from "./components/MainCarousel";
+
+function App() {
+  return <div className="App"><MainCarousel/></div>;
+}
+
+export default App;
---
+++ frontend/src/components/MainCarousel.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import AmbonImage from "../images/Ambon.jpg"; // You need to create this component or replace it with your image component.
+
+function MainCarousel() {
+  return (
+    <Carousel style={{maxHeight:"80vh"}} fade>
+      <Carousel.Item>
+        <img style={{ width: '100%', height: '80vh', objectFit:"cover" }} src={AmbonImage} />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img style={{ width: '100%', height: '80vh', objectFit:"cover" }} src={AmbonImage} />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img style={{ width: '100%', height: '80vh', objectFit:"cover" }} src={AmbonImage} />{" "}
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default MainCarousel;
