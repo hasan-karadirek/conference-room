e9982e90ac7491a8d4dc8937188e628044da2b5d
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2023-12-02 12:25:14 +0100

    navbar created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import MainCarousel from "./components/MainCarousel";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <div className="App"><MainCarousel/></div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <MainCarousel />
+    </div>
+  );
 }
 
 export default App;
